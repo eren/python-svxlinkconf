db6ec4766c98cef9d452fab176f8edf2a294bb74
Eren Türkay <turkay.eren@gmail.com>
2011-09-08 16:26:28 +0300

    Web: Add web branch and initial commit.
    The web interface is a Django interface. It will manage
    creating/deleting/updating nodes and starting/stopping and restarting
    svxlink server

---
+++ web/svxconf/views.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+
+def home(request):
+    return render_to_response("home.html")
+
+
+def about(request):
+    return render_to_response("about.html")
